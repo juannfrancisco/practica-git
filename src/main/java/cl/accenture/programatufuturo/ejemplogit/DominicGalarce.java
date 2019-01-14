e5601d49910fe0ba410b19369835fa1392b60f44
DominicGalarce <domi.galarce@gamail.com>
2019-01-14 11:03:08 -0300

    sout Dominic Galarce

---
+++ src/main/java/cl/accenture/programatufuturo/ejemplogit/DominicGalarce.java
@@ -0,0 +1,9 @@
+package cl.accenture.programatufuturo.ejemplogit;
+
+public class DominicGalarce {
+
+
+    public static void main(String[] args){
+        System.out.println("Hola mi nombre es Dominic");
+    }
+}
