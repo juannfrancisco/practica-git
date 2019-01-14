a8d7922d5813316be5328bcf8e9693da5d96eb0d
macarena.abarca <macarena.abarca18@hotmail.com>
2019-01-14 11:37:22 -0300

    lalala

--- src/main/java/cl/accenture/programatufuturo/ejemplogit/MacarenaAbarca.java
+++ src/main/java/cl/accenture/programatufuturo/ejemplogit/MacarenaAbarca.java
@@ -2,6 +2,6 @@ package cl.accenture.programatufuturo.ejemplogit;
 
 public class MacarenaAbarca {
     public static void main(String[] args) {
-        System.out.println("Hola, mi nombre es Macarena Abarca");
+        System.out.println("Hola, mi nombre es Macarena Abarca lalala");
     }
 }
