511522ccc17b4e32bba69d6a8ffaa5a3df86d1d3
Rogelio Cifuentes <rogelio.cifuentes@gmail.com>
2019-01-14 10:58:37 -0300

    Rogelio Cifuentes

---
+++ src/main/java/cl/accenture/programatufuturo/ejemplogit/RogelioCifuentes.java
@@ -0,0 +1,12 @@
+package cl.accenture.programatufuturo.ejemplogit;
+
+public class RogelioCifuentes {
+
+    private static final String nombre = "Rogelio";
+    private static final String apellido = "Cifuentes";
+
+    public static void main(String[] args) {
+        System.out.println(nombre +" " + apellido);
+    }
+
+}
