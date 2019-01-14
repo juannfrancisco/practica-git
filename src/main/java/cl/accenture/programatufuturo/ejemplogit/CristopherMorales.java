93bd5ca4e8872fee747455c70bf5ed7385ebf84d
toffer <cristopher.moralesceron@outlook.com>
2019-01-14 10:59:29 -0300

    se agrega clase

---
+++ src/main/java/cl/accenture/programatufuturo/ejemplogit/CristopherMorales.java
@@ -0,0 +1,7 @@
+package cl.accenture.programatufuturo.ejemplogit;
+
+public class CristopherMorales {
+    public static void main(String[] args) {
+        System.out.println("Hola mi nombre es Cristopher Morales");
+    }
+}
