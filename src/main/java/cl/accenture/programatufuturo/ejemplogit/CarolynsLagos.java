cec9709d88e0f161c67e6074ba1b5b2bfd9132e3
Carolyns-Lagos <carolyns.echegaray@gmail.com>
2019-01-14 11:00:33 -0300

    Saludo caro

---
+++ src/main/java/cl/accenture/programatufuturo/ejemplogit/CarolynsLagos.java
@@ -0,0 +1,9 @@
+package cl.accenture.programatufuturo.ejemplogit;
+
+public class CarolynsLagos {
+    public static void main(String[] args) {
+        System.out.println("Hola mi nombre es Carolyns Lagos :3");
+    }
+}
+
+
