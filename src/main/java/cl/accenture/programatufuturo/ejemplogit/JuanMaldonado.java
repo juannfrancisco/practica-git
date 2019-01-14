8805760e7fd8c210f60a25cdfcf4ae072d05e649
Juan Francisco Maldonado Leon <juan.maldonado.leon@gmail.com>
2019-01-14 09:13:45 -0300

    inicio de proyecto

---
+++ src/main/java/cl/accenture/programatufuturo/ejemplogit/JuanMaldonado.java
@@ -0,0 +1,9 @@
+package cl.accenture.programatufuturo.ejemplogit;
+
+public class JuanMaldonado {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola mi nombre es Juan Maldonado");
+    }
+}
